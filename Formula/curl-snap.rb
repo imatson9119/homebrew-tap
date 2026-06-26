fd6b48ca74cdd74f91a4984ce302703165ddbd2f
Ian Matson <imatson9119@gmail.com>
2026-06-26 10:55:13 -0500

    curl-snap 2.5.2

--- Formula/curl-snap.rb
+++ Formula/curl-snap.rb
@@ -1,8 +1,8 @@
 class CurlSnap < Formula
   desc "Render a curl request and response as a clean, shareable image"
   homepage "https://github.com/imatson9119/curl-snap"
-  url "https://registry.npmjs.org/curl-snap/-/curl-snap-2.5.1.tgz"
-  sha256 "3a3c172762b18e1ecb293e65ff8029aa0b030f31153d5111874f6f757c0efc8c"
+  url "https://registry.npmjs.org/curl-snap/-/curl-snap-2.5.2.tgz"
+  sha256 "b5463c5bd296a5a31c312fae3d7c0e920c0338754657f5de546f605adb3c5442"
   license "MIT"
 
   depends_on "node"
